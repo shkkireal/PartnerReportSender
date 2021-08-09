94536a852fd292dedb187d797482d45dd66876e9
Блинов <C:\Users\\AppData\Roaming\The Bat!>
2021-08-09 12:22:24 +0300

    1.03 add protect fields reguired with JS and change css bootstrap class

--- classes/formPartnerSender.js
+++ classes/formPartnerSender.js
@@ -1,6 +1,7 @@
 $(document).ready(function($) {
 	
 
+
 	$("#PartnerLid").submit(function() { return false; });
 
 		
@@ -9,27 +10,103 @@ $(document).ready(function($) {
 			var emailval  = $("#email").val();
 			var epbval  = $("#epb").val();
 			var telefonval  = $("#telefon").val();
-			var acceptval    = $("#accept").val();
+			var acceptval    = document.getElementById('accept');
 			var namelen    = nameval.length;
 			var emaillen    = emailval.length;
 			var epblen      = epbval.length;
 			var telefonlen    = telefonval.length;
+			var validate    = 0;
 		    
 							
-		
+		    console.log(telefonlen);
 			
 			
-			if(namelen < 4) {
-				$("#name").addClass("error");
+			if(namelen < 8) {
+				
+				$("#name").removeClass("is-valid");
+				$("#name").addClass("is-invalid");
+				console.log(namelen);
+
+				
+				validate--;
 				
 			}
-		
+			else{
+				$("#name").removeClass("is-invalid");
+				$("#name").addClass("is-valid");
+				validate++;
+				
+			}
+			
+		    if(emaillen < 4) {
+				$("#email").removeClass("is-valid");
+				$("#email").addClass("is-invalid");
+				console.log(emaillen);
+			    validate--;
+			}
+			else{
+				
+				 var re = /^[\w-\.]+@[\w-]+\.[a-z]{2,4}$/i;
+                 var valid = re.test(emailval);
+				 if (valid){
+				$("#email").removeClass("is-invalid");
+				$("#email").addClass("is-valid");
+				validate++;
+				 }
+				 else {
+					 $("#email").removeClass("is-valid");
+				     $("#email").addClass("is-invalid");
+					 validate--;
+				 }
+			}
+			if(epblen < 16) {
+				$("#epb").removeClass("is-valid");
+				$("#epb").addClass("is-invalid");
+				console.log(epblen);
+			validate--;
+			}
+			else{
+				$("#epb").removeClass("is-invalid");
+				$("#epb").addClass("is-valid");
+				validate++;
+				
+			}
+			if(telefonlen < 11) {
+				
+				$("#telefon").removeClass("is-valid");
+				$("#telefon").addClass("is-invalid");
+				console.log(telefonlen);
+			validate--;
+			}
+			else{
+				
+				$("#telefon").removeClass("is-invalid");
+				$("#telefon").addClass("is-valid");
+				validate++;
+				
+			}
+			if(acceptval.checked) {
+				
+				$("#accept").removeClass("is-invalid");
+				$("#accept").addClass("is-valid");
+				console.log("acc on");
+				validate++;
+				
+			}
+			else{
+				$("#accept").removeClass("is-valid");
+				$("#accept").addClass("is-invalid");
+				
+			validate--;
+				
+			}
 					
 			
-			
-			if(namelen >= 4) {
+			console.log("validate");
+				console.log(validate);
+			if(validate == 5) {
 				
-				console.log(namelen);
+				console.log("mailed");
 				$("#send").replaceWith("<em>отправка...</em>");
 				
 				$.ajax({
